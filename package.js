ebd23203812125fc2e9c6c32eb410888675a86c4
Barrabin Fc. ⚑ <barrabin.fc@gmail.com>
2014-09-01 15:21:21 -0300

    Upadting package

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
-    summary: "Refills -  Prepackaged patterns and components, built on top of Bourbon, Bitters and Neat"
-    version: "0.1",
+    summary: "Refills -  Prepackaged patterns and components, built on top of Bourbon, Bitters and Neat",
+    version: "0.0.1",
     name: "bbfc:refills",
     git: "https://github.com/barrabinfc/meteor-refills"
 });
@@ -8,12 +8,14 @@ Package.describe({
 Package.onUse(function (api) {
     api.versionsFrom("METEOR@0.9.0");
     api.use(['fourseven:scss@0.9.5','ciara:bourbon@3.1.8', 
-             'ciara:neat@1.5.0','ciara:bitters@0.10.0'], ['client', 'server']);
+             'ciara:neat@1.5.0','ciara:bitters@0.10.0',
+             'jquery@1.0.0'], ['client', 'server']);
 
     api.add_files( [
                      'refills/modal.scss','refills/video.scss',
-                     'refills/dropdown.scss','refills/dropdown.js'
+                     'refills/dropdown.scss'
                     ],
                    'server');
-    //dont need to add any files to the client, they're there just so the package will build
+
+    api.add_files( ['refills/dropdown.js',], 'client' )
 });
